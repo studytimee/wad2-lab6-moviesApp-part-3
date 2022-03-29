35e3199fafd9470f4a6055a1d512f34765ad69ad
Abid Ali <studytimee@gmail.com>
2022-03-29 18:23:05 +0100

    Full Review Page
    In the browser:
    
    Go to the home page and click on a movie.
    On the movie details page, click the Reviews button.
    Click the 'Full Review' link of one of the review excerpts.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import MovieReviewPage from "./pages/movieReviewPage";
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Redirect, Switch, Link  } from "react-router-dom";
+import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favoriteMoviesPage"; // NEW
@@ -17,6 +18,7 @@ const App = () => {
         </li>
       </ul>
       <Switch>
+        <Route path="/reviews/:id" component={MovieReviewPage} />
         <Route exact path="/movies/favourites" component={FavouriteMoviesPage} />
         <Route path="/movies/:id" component={MoviePage} />
         <Route exact path="/" component={HomePage} />
