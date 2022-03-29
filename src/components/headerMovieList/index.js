54a9b6d42e03380fc77ea1e94c15d1489349e5e8
Abid Ali <studytimee@gmail.com>
2022-03-29 18:46:26 +0100

    Fix backward/foeward arrows

--- src/components/headerMovieList/index.js
+++ src/components/headerMovieList/index.js
@@ -5,6 +5,9 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
+import { useHistory } from 'react-router-dom'
+
+const history = useHistory(); 
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,18 +24,14 @@ const Header = (props ) => {
   const title = props.title
   return (
     <Paper component="div" className={classes.root}>
-      <IconButton
-        aria-label="go back"
-      >
+       <IconButton aria-label="go back" onClick={() => history.goBack()}>
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
 
       <Typography variant="h4" component="h3">
         {title}
       </Typography>
-      <IconButton
-        aria-label="go forward"
-      >
+      <IconButton aria-label="go forward" onClick={() => history.goForward()}>
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
     </Paper>
